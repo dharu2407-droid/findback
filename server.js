0797baccd028cd96ce8848ec8bc25dbf7f6ae79f
dharu2407-droid <dharu2407@gmail.com>
2026-09-01 11:47:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ module.exports = app;
 
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
-
+  
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
